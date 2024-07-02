81c7a6363c254131899056aa269da1e0a80b3cc8
divraze <divraze@mail.ru>
2024-07-02 10:50:01 +0400

    update game_process.py

--- brain_games/game_process.py
+++ brain_games/game_process.py
@@ -2,9 +2,7 @@ import prompt
 
 
 def game_process(question_answer, manual):
-    name = prompt.string(
-f'''Welcome to the Brain Games!
-May I have your name? ''')
+    name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
 
     print(f'Hello, {name}!')
     print(f'{manual}')
@@ -13,16 +11,15 @@ May I have your name? ''')
 
     while i < 3:
         question, answer = question_answer()
-        user_answer = prompt.string(
-    f'''Question: {question}
-Your answer: '''
-        )
+        user_answer = prompt.string(f'Question: {question}\nYour answer: ')
 
         if user_answer == str(answer):
             i += 1
             print('Correct!')
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'.\nLet's try again, {name}!")
+            print(
+                f"'{user_answer}' is wrong answer ;(."
+                f" Correct answer was '{answer}'.\nLet's try again, {name}!")
             break
     if i == 3:
         print(f'Congratulations, {name}!')
